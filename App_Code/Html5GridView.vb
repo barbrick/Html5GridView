e473851cf14146ad42e21450fbe00c34d66e37d8
Trevor Barbrick <trevor@barbrick.com>
2013-10-08 21:09:41 -0300

    Initial Commit

---
+++ App_Code/Html5GridView.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.VisualBasic
+
+<Assembly: TagPrefix("ActiveServerControls", "asp")> 
+Namespace ActiveServerControls
+    < _
+    ToolboxData("<{0}:Html5GridView ID='Html5GridViewID' runat=""server""> </{0}:Html5GridView>")> _
+    Public Class Html5GridView
+        Inherits GridView
+
+        ' Create a new class that inherits HtmlTextWriter to modify the rendering of invalid attributes
+        Private Class Html5ReWriter
+                Inherits HtmlTextWriter
+
+            Public Sub New(writer As HtmlTextWriter)
+                MyBase.New(writer)
+            End Sub
+
+            Public Overrides Sub AddAttribute(key As HtmlTextWriterAttribute, value As String)
+                ' Strip out attributes which cause HTML5 Validation Errors
+                If (key.Equals(HtmlTextWriterAttribute.Rules)) Then
+                    Return
+                End If
+
+                If (key.Equals(HtmlTextWriterAttribute.Cellspacing)) Then
+                    Return
+                End If
+
+                If (key.Equals(HtmlTextWriterAttribute.Border)) Then
+                    Return
+                End If
+
+                MyBase.AddAttribute(key, value)
+            End Sub
+
+            Protected Overrides Function OnTagRender(name As String, key As HtmlTextWriterTag) As Boolean
+                ' Remove unnecessary span tags which cause HTML5 Validation Errors
+                If (key.Equals(HtmlTextWriterTag.Span)) Then
+                    Return False
+                End If
+                Return MyBase.OnTagRender(name, key)
+            End Function
+
+        End Class
+
+        ' Override the render method of the GridView to use the modified HtmlTextWriter
+        Protected Overrides Sub Render(ByVal writer As HtmlTextWriter)
+            MyBase.Render(New Html5ReWriter(writer))
+        End Sub
+    End Class
+
+End Namespace
